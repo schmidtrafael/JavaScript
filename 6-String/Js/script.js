5ba330d80ac08bc46f12d30b66b7c06750a48419
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-10 20:49:13 -0300

    Update
    Mais aulas

---
+++ 6-String/Js/script.js
@@ -0,0 +1,26 @@
+var nome = "Rafael";
+var sobreNome = "Schmidt";
+
+var nomeCompleto = nome + " " + sobreNome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(nomeCompleto);
+
+var frase = 'Esta é uma frase';
+
+console.log(nome + " " + frase + " " + sobreNome);
+
+//escrevendo direto no HTML
+document.write('Eu disse "olá!"');
+
+console.log("Nome: " + nome);
+
+document.write('"Salve Salve Familia!"');
+
+
+
+
