12dacae3c4f8714cee05aa11bf798c481c1b63a0
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-10 20:01:04 -0300

    update

--- 3-Particularidades/Js/script.js
+++ 3-Particularidades/Js/script.js
@@ -3,10 +3,18 @@ console.log("primeiro");
 console.log("segundo");
 
 console.log("Mais um console.log");
-
+//declara variavel
 var a =1;
 
+//variavel recebe teste
 a = 'teste';
 
 console.log(a);
+
+// Comentario de uma linha
+
+/*
+multiplas
+linhas
+*/
 
---
+++ 4-Variaveis/Js/script.js
@@ -0,0 +1,10 @@
+var teste = 1;
+
+var teste = 'Rafael';
+
+console.log(teste);
+
+var teste = 'Schmidt';
+
+console.log(teste);
+
