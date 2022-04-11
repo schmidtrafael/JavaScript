3bfa0b9b033102183c371d54126a6d05b387aa04
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-10 21:52:44 -0300

    Update script.js

--- 9-objetos/Js/script.js
+++ 9-objetos/Js/script.js
@@ -1,17 +1,24 @@
-// Hoisting - içamento
+var obj = {
+    nome: "Rafael",
+    idade: 29,
+    profissao: "Programador",
+    estaTrabalhando: true,
+};
 
-/*Carrega as variaveis porem ainda nao 
-sabe o que foi atibuido pois isso executa no
-cabeçalho do codigo*/
+console.log(obj);
 
-console.log(sobrenome);
+console.log(typeof obj);
 
-//aqui atribuimos o valor
-var sobrenome = "Schmidt";
-//agora vaaai!
-console.log(sobrenome);
+console.log(obj.nome);
+console.log(obj.idade);
+console.log(obj.profissao);
 
+console.log("O meu nome é " + obj.nome);
 
-var nome = null;
+console.log("Meu nome: " + obj.nome + ", " + "Minha idade: " + obj.idade + ", " + "Minha profissão: " + obj.profissao);
 
-console.log(nome)
+obj.nome = "Felipe";
+obj.idade = 98;
+obj.profissao = "Lutador de MMA";
+
+console.log("Meu nome: " + obj.nome + ", " + "Minha idade: " + obj.idade + ", " + "Minha profissão: " + obj.profissao);
