96235340f93696d6bf1a7b0e9cb21994c60394ec
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-11 10:53:43 -0300

    update

---
+++ 10-array/Js/script.js
@@ -0,0 +1,20 @@
+var arr = [5, "Rafael", true, {teste, teste:2}];
+
+console.log(arr);
+
+var arr2 = [2,3,4,5,6];
+
+console.log(arr2);
+
+console.log(arr[1]);
+console.log(arr2[0]);
+
+arr[4] = 10;
+
+arr[0] = "teste";
+
+console.log(arr);
+
+console.log(typeof arr);
+
+
