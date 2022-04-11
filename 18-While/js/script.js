aac5fb9a0f1bd9bafbb273a00d9672ac8a17b1fa
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-11 20:49:24 -0300

    update

---
+++ 18-While/js/script.js
@@ -0,0 +1,18 @@
+var x = 0;
+
+while (x <= 5){
+    console.log("Testando repetição " + x);
+
+//incrementa
+x++;
+
+}
+
+
+var arr = ["Eu", "nós", "eles", "ele"]
+var y=0;
+
+while (y<=3){
+    console.log(arr[y]);
+    y++;
+}
