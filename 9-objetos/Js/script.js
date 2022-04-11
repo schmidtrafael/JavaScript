089bd483ca17b09aa85f14f928b41256c9257290
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-10 21:38:00 -0300

    Create script.js

---
+++ 9-objetos/Js/script.js
@@ -0,0 +1,17 @@
+// Hoisting - içamento
+
+/*Carrega as variaveis porem ainda nao 
+sabe o que foi atibuido pois isso executa no
+cabeçalho do codigo*/
+
+console.log(sobrenome);
+
+//aqui atribuimos o valor
+var sobrenome = "Schmidt";
+//agora vaaai!
+console.log(sobrenome);
+
+
+var nome = null;
+
+console.log(nome)
