e449b6fc230217c5c4cbd5433435756c2c05914b
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-11 11:32:56 -0300

    updates

---
+++ 11-if/Js/script.js
@@ -0,0 +1,9 @@
+var idade = 19;
+
+console.log("Antes do IF");
+
+if(idade > 18){
+    console.log("Maior de 18");
+}
+
+console.log("Depois do If");
---
+++ 12-Operadores-de-comparação/Js/script.js
@@ -0,0 +1,24 @@
+var idade = 18;
+//maioor que
+if(idade >= 18){
+    console.log("Maior de 18");
+}
+//menor que
+if(idade <= 17){
+    console.log("Menor de 18");
+}
+// igual a... ==
+//atribui =
+
+
+//Se igual a...
+var nome = "Matheus"
+if(nome == "Matheus"){
+    console.log("Meu nome é: ", nome);
+}
+
+if(nome != "Matheus"){
+    console.log("Oo nome é outro");
+}
+
+
