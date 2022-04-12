649a0764322b442d9e658ce918a89b440d26b943
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-11 23:03:27 -0300

    Update script.js

---
+++ 20-For/js/script.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 10; i++){
+    console.log("Repetindo for: "+ i)
+}
+
+var arr = [1,2,3,4];
+
+for(var j = 0; j < arr.length; j++){
+    console.log(arr[j]);
+}
+
+for(var x = 50; x < 100; x *= 2){
+    console.log(x);
+}
