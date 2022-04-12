ec94db5ef1c01d2fcc79dd7e184622894738d5e3
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-11 21:14:02 -0300

    ok

---
+++ 18-While copy/js/script.js
@@ -0,0 +1,26 @@
+var x = 0;
+
+while (x <= 5){
+    console.log("Testando repetição " + x);
+
+//incrementa
+x++;
+
+}
+
+
+var arr = ["Eu", "nós", "eles", "ele"]
+var y=0;
+
+while (y<=3){
+    console.log(arr[y]);
+    y++;
+}
+
+var nome = "Rafael";
+var y=0;
+
+while (y<=5){
+    console.log(nome[y]);
+    y++;
+}
---
+++ 19-atribuicao/js/script.js
@@ -0,0 +1,35 @@
+var x = 1;
+var y = 2;
+
+//Soma
+console.log(x = x + y);
+console.log(x+=y);
+
+//Subtração
+console.log(x-=y);
+
+//Multiplicação
+console.log(x*= y);
+
+//Divisão
+console.log(x /= y);
+
+//Loops
+console.log(x++);
+console.log(x--);
+
+while(x <= 100){
+
+    console.log(x);
+
+    x *= 2;
+}
+console.log(x);
+
+var j = 20;
+
+while (x > 0 ){
+    console.log(x);
+
+    x -= j;
+}
