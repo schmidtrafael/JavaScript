fdbf9eda3a56e8015d9d6a81e1e745386fa3fc8f
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-19 16:19:44 -0300

    update

---
+++ 29-metodosArray/js/script.js
@@ -0,0 +1,39 @@
+//lenght
+var arr;
+
+arr = [1,2,3,4,5];
+
+console.log(arr.lenght);
+
+// push
+arr.push(6);
+
+console.log(arr);
+
+// pop
+arr.pop();
+
+console.log(arr);
+
+// unshift
+arr.unshift(0);
+
+console.log(arr);
+
+// shift
+
+arr.shift(0);
+
+console.log(arr);
+
+// acessar ultimo elemento
+
+console.log(arr[length]);
+
+// isarray
+console.log(Array.isArray(5));
+
+console.log(Array.isArray(arr));
+
+
+
