ba97bc8e1f7aa7a76b4090bfb7daa1b6dc7592de
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-22 16:03:21 -0300

    ok

---
+++ 43-Callback functions/js/script.js
@@ -0,0 +1,8 @@
+// Selecionando elemento
+var title = document.querySelector("#title");
+
+// Adiciona o estilo
+title.style.color = "green";
+
+// Background Color
+title.style.backgroundColor = "black";
