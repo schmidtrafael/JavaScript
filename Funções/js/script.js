f8ebc23feb9ec7e54c43cc947e7326ddfacdb178
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-25 11:42:26 -0300

    Criação de pasta para Funções
    Anotando e refazendo exercicios em aula para exercitar funções em JS

---
+++ Funções/js/script.js
@@ -0,0 +1,14 @@
+/*
+Função para exemplo kusando par ou impar.
+*/
+function parOuImpar(numeroDesejado) {
+    if (n%2==0) {
+        return ("Par")
+    } else {
+        return ("Impar")
+    }
+}
+
+let resultadoFinal = parOuImpar(11);
+
+console.log(parOuImpar);
