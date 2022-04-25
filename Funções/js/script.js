036356ca4444e539a401b0cb11f893ed757d9e0a
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-25 12:46:29 -0300

    Update script.js
    Função para multiplicar valor, colocando a função direto em
    umaa variável

--- Funções/js/script.js
+++ Funções/js/script.js
@@ -18,7 +18,15 @@ entradas o valor já estará no padrão ZERO.
 function somaValores(n1=0, n2=0) {
 
     return n1 + n2;
-    
+
 }
 
 console.log(somaValores(7));
+/*
+Função para multiplicar valor, colocando a função direto em
+umaa variável
+*/
+let v = function(x) {
+    return x*2
+}
+console.log(v(5));
