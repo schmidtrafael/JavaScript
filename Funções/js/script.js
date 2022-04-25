0803a0f695b0c9bf1b46b1b5dd97a0d6b34f5c42
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-25 11:44:25 -0300

    Update script.js

--- Funções/js/script.js
+++ Funções/js/script.js
@@ -1,14 +1,14 @@
 /*
-Função para exemplo kusando par ou impar.
+Função para exemplo usando par ou impar.
 */
 function parOuImpar(numeroDesejado) {
-    if (n%2==0) {
+    if (numeroDesejado%2==0) {
         return ("Par")
     } else {
         return ("Impar")
     }
 }
 
-let resultadoFinal = parOuImpar(11);
+let resultadoFinal = parOuImpar(6);
 
-console.log(parOuImpar);
+console.log(resultadoFinal);
