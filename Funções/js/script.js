da7db3f7cba27b178dc42f7f5f4eb9c492068fb4
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-25 12:42:28 -0300

    Update script.js

--- Funções/js/script.js
+++ Funções/js/script.js
@@ -9,6 +9,16 @@ function parOuImpar(numeroDesejado) {
     }
 }
 
-let resultadoFinal = parOuImpar(6);
+console.log(parOuImpar(6));
+console.log("____________________________")
+/*
+Função para somar dois valores e caso ZERO em uma das 
+entradas o valor já estará no padrão ZERO.
+*/
+function somaValores(n1=0, n2=0) {
 
-console.log(resultadoFinal);
+    return n1 + n2;
+    
+}
+
+console.log(somaValores(7));
