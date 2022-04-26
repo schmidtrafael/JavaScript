0a03df56e62cf0ddcdf742b7c7b587ec5409b1dc
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-26 11:44:33 -0300

    Update script.js

--- 47-Event Click/js/script.js
+++ 47-Event Click/js/script.js
@@ -5,5 +5,8 @@ var btn = document.querySelector("#btn");
 console.log(btn);
 
 btn.addEventListener("click", function(){
-    
-})
+    console.log("Clicado");
+    console.log(this);
+    this.style.color = "red";
+});
+
