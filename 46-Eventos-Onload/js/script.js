ffe4ff4dc0c107bc2d5e6783817673fdeec72198
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-26 11:34:31 -0300

    On load

---
+++ 46-Eventos-Onload/js/script.js
@@ -0,0 +1,13 @@
+//evento
+
+window.onload = function() {
+
+    console.log("Carrega o DOM");
+
+    var title = document.querySelector("#title");
+
+    console.log(title);
+
+}
+
+console.log("teste")
