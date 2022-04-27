cd9ac43b6c875d60d81cc625a0379d41de37bca7
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-27 14:25:14 -0300

    Create script.js

---
+++ 48-MouseOver_MouseOut/js/script.js
@@ -0,0 +1,19 @@
+// Mouseover
+
+var title = document.querySelector("#title");
+
+title.addEventListener("mouseover", function() {
+    
+    this.style.backgroundColor = "yellow";
+
+});
+
+// mouseout
+
+var title = document.querySelector("#title");
+
+title.addEventListener("mouseout", function() {
+    
+    this.style.backgroundColor = "white";
+
+});
