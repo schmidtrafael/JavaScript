f573478b5753b8ad826272199b77fa9b9a3fec22
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-27 14:29:57 -0300

    Update script.js

--- 48-MouseOver_MouseOut/js/script.js
+++ 48-MouseOver_MouseOut/js/script.js
@@ -17,3 +17,22 @@ title.addEventListener("mouseout", function() {
     this.style.backgroundColor = "white";
 
 });
+
+// Afetar outro elemento com mouseover
+var subtitle = document.querySelector(".subtitle");
+
+subtitle.addEventListener("mouseover", function() {
+
+    var legenda = document.querySelector("#legenda");
+
+    legenda.classList.remove("hide");
+
+});
+
+subtitle.addEventListener("mouseout", function() {
+
+    var legenda = document.querySelector("#legenda");
+
+    legenda.classList.add("hide");
+
+});
