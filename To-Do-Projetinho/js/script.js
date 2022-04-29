3eb18fbb8c89926e563f68038d1d4c19c1509a75
Rafael <48494280+schmidtrafael@users.noreply.github.com>
2022-04-29 11:19:30 -0300

    Update script.js

--- To-Do-Projetinho/js/script.js
+++ To-Do-Projetinho/js/script.js
@@ -1,3 +1,42 @@
+// Função que adiciona tarefa
+function addTask() {
+
+    //Titulo da tarefa
+    const taskTitle = document.querySelector("#task-title").value;
+
+    if (taskTitle) {
+
+        // Clona template
+        const template = document.querySelector(".template");
+
+        const newTask = template.cloneNode(true);
+
+        // adiciona titulo
+        newTask.querySelector(".task-title").textContent = taskTitle;
+
+        //Remover as classes desnecessarias
+        newTask.classList.remove("template");
+        newTask.classList.remove("hide");
+
+        //Adiciona tarefa na lista
+        const list = document.querySelector("#task-list");
+
+        list.appendChild(newTask);
+
+        // Limpar texto
+        document.querySelector("#task-title").value = "";
+    }
+}
+
+
+
+// Evento adicionando tarefas
 const addBtn = document.querySelector("#add-btn");
 
-addBtn.addEventListener("click")
+addBtn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    addTask();
+
+});
